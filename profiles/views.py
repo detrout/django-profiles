154f04579bfc8e4325434820e3d2e4509212c457
Diane Trout <diane@debian.ghic.org>
2017-02-12 14:50:54 -0800

    Forgot to commit impor for get_user_model

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ Views for creating, editing and viewing site-specific user profiles.
 
 """
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
